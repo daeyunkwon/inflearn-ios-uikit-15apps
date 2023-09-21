366c75c9f8884d6cc66aa12c48018e58110a8419
Daeyun <sweet_ray@naver.com>
2023-09-21 15:57:03 +0900

    Add: ToDoApp.xcodeproj
    이전 프로젝트 소멸로 인해...강의용 프로젝트 올림ㅠ

---
+++ 12. ToDoApp/ToDoData+CoreDataProperties.swift
@@ -0,0 +1,35 @@
+//
+//  ToDoData+CoreDataProperties.swift
+//  MyToDoApp
+//
+//  Created by Allen H on 2022/04/21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoData {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoData> {
+        return NSFetchRequest<ToDoData>(entityName: "ToDoData")
+    }
+
+    @NSManaged public var memoText: String?
+    @NSManaged public var date: Date?
+    @NSManaged public var color: Int64
+
+    var dateString: String? {
+        let myFormatter = DateFormatter()
+        myFormatter.dateFormat = "yyyy-MM-dd a hh:mm"
+        myFormatter.locale = Locale(identifier: "ko_KR")
+        guard let date = self.date else { return "" }
+        let savedDateString = myFormatter.string(from: date)
+        return savedDateString
+    }
+}
+
+extension ToDoData : Identifiable {
+
+}
